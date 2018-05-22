3d0de6674b92396dd2963c1e3c950073b7c6ed81
George Daramouskas <george.daramouskas@gmail.com>
2018-05-22 21:18:24 +0200

    [PORT] Backport KeyboardNavigationMixin functionality from 10.0

---
+++ web_widget_keyboard_navigation_mixin/__openerp__.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+# © 2018 Therp BV <http://therp.nl>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+{
+    "name": 'web_widget_keyboard_navigation_mixin',
+    "version": "8.0.0.2.0",
+    "description": "",
+    "depends": [
+        'base',
+        'web',
+    ],
+    'qweb': [
+        'static/src/xml/base.xml',
+    ],
+    'license': 'AGPL-3',
+    'data': [
+        'views/webclient_templates.xml',
+    ],
+    "author": "Odoo SA, Odoo Community Association (OCA), Therp BV",
+    "installable": True,
+}
